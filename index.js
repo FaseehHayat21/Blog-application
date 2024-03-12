ca722295b82b5d5e2b7d4986363729c02f1ddebb
FaseehHayat21 <faseehhayat999@gmail.com>
2024-03-12 20:14:42 +0500

    Helmety added

--- index.js
+++ index.js
@@ -2,11 +2,20 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const path = require('path')
+const helmet = require('helmet');
 const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(express.json({ limit: '10mb' }));
 
+app.use(
+    helmet.contentSecurityPolicy({
+      directives: {
+        defaultSrc: ["'none'"],
+        imgSrc: ["'self'", "data:"], // Allow loading images from 'self' and 'data' URIs
+      },
+    })
+  );
 mongoose.connect('mongodb://127.0.0.1:27017').then(
     app.listen(1000, ()=>{
         console.log("Blog Application Server is Running")
