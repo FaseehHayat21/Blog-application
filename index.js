f41c886a99f39d4d8142cd78eb29e3cd70777e17
Faseeh Hayat <139529358+FaseehHayat21@users.noreply.github.com>
2024-06-30 14:20:16 +0500

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,16 @@ app.use(cors())
 app.use(express.json())
 app.use(express.json({ limit: '10mb' }));
 
+
+app.use(cors(
+    {
+        origin: ["https://blog-application-pied.vercel.app/"],
+        methods: ["POST", "GET"],
+        credentials: true
+    }
+    )
+    )
+            
 app.use(
     helmet.contentSecurityPolicy({
       directives: {
